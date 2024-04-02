f3522eb46ac183190b7a61472550c7b15b70ba3d
vsalfafara <alfafara.vm@gmail.com>
2024-04-02 14:12:30 +0800

    Added docker files

--- db.php
+++ db.php
@@ -2,17 +2,17 @@
 
 class Connection {
   public $connection;
-  private $host = "localhost";
-  private $dbname = "tasks-db";
-  private $user = "root";
-  private $pass = "";
+  private $host = "dpg-co5pi4cf7o1s73a5d0k0-a";
+  private $dbname = "task_manager_db_a8i2";
+  private $user = "vsalfafara";
+  private $pass = "pcYh6GS2ATjp6s3C4w6eTumXZzL07AEz";
   private $charset = "utf8mb4";
   private $options = [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
   ];
 
   public function __construct() {
-    $dsn = "mysql:host=$this->host;dbname=$this->dbname;charset=$this->charset";
+    $dsn = "pgsql:host=$this->host;dbname=$this->dbname";
     $this->connection = new PDO($dsn, $this->user, $this->pass, $this->options);
   }
 
